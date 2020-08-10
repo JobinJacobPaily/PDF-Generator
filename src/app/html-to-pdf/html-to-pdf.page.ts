e00ce51353d437eccf95907d362c21472bbebcfb
jobinjacobpaily <jobinjacobpaily@gmail.com>
2020-08-10 14:01:47 +0530

    Success Pdf generation using cordova-pdf-generator

--- src/app/html-to-pdf/html-to-pdf.page.ts
+++ src/app/html-to-pdf/html-to-pdf.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {PDFGenerator} from '@ionic-native/pdf-generator/ngx'
+import {PDFGenerator} from '@ionic-native/pdf-generator/ngx';
+import {File} from '@ionic-native/file/ngx';
+import {FileOpener} from '@ionic-native/file-opener/ngx';
 
 @Component({
   selector: 'app-html-to-pdf',
@@ -8,7 +10,11 @@ import {PDFGenerator} from '@ionic-native/pdf-generator/ngx'
 })
 export class HtmlToPdfPage implements OnInit {
 
+  pdfcontainer;
+
   constructor(
+    public file :File,
+    public fileOpener :FileOpener,
     public pdfGenerator:PDFGenerator
   ) { }
 
@@ -17,14 +23,36 @@ export class HtmlToPdfPage implements OnInit {
 
   pdfgenerator()
   { 
-    var url ='www.google.com'
+    var url ='https://www.google.com';
+    const data  = document.getElementById('hello');
     var options ={
           documentSize: 'A4',
           type: 'base64'
                  }
 
-    this.pdfGenerator.fromURL(url, options).then(base64String => {
-      
-      console.log(base64String)});
+    this.pdfGenerator.fromData(data.outerHTML, options).then(base64String => {
+
+      // this.pdfcontainer = new Blob([base64String], {type :'application/pdf'});
+     // this.pdfcontainer = base64String
+      console.log(base64String);
+      fetch('data:application/pdf;base64,'+ base64String,
+      {
+        method :'post'
+      }).then((res)=>res.blob()).then((blob) =>{this.pdfcontainer = blob; console.log('blob created');})
+    });
   }
+
+  writeFile()
+   {
+    
+    this.file.writeFile(this.file.externalDataDirectory,'file.pdf',this.pdfcontainer,{replace : true})
+    .then(_ => console.log('File written successfully'));
+   }
+
+  openfile()
+   {
+     this.fileOpener.open(this.file.externalDataDirectory+'file.pdf','application/pdf')
+     .then( _ => console.log('opnening pdf'));
+   }
+  
 }
